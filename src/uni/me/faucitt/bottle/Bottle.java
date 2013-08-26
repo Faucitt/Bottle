aff727cd89c3c959edf85a9705b00d811358b99a
Dan <dan@danthekarman.tk>
2013-08-25 21:25:43 -0700

    Commands + CommandList

--- src/uni/me/faucitt/bottle/Bottle.java
+++ src/uni/me/faucitt/bottle/Bottle.java
@@ -3,15 +3,25 @@ package uni.me.faucitt.bottle;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.command.Command;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import uni.me.faucitt.bottle.commands.CommandExecutor;
 import uni.me.faucitt.bottle.exceptions.BottleException;
 
 public class Bottle extends JavaPlugin {
 	
 	public static List<BottleException> bottleExceptions = new ArrayList<BottleException>();
+	public static List<Command> commands = new ArrayList<Command>();
 	
 	public void onEnable() {
+		Command commandBottle = null;
+		commandBottle.setLabel("bottle");
+		commandBottle.setDescription("Gives a list of all Bottle commands");
+		
+		commands.add(commandBottle);
+		
+		getCommand("bottle").setExecutor(new CommandExecutor(this));
 	}
 	
 	public void onDisable() {
---
+++ src/uni/me/faucitt/bottle/commands/CommandExecutor.java
@@ -0,0 +1,35 @@
+package uni.me.faucitt.bottle.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+import uni.me.faucitt.bottle.Bottle;
+
+public class CommandExecutor implements org.bukkit.command.CommandExecutor {
+	
+	private Bottle plugin;
+	
+	public Bottle getPlugin() {
+		return plugin;
+	}
+
+	public void setPlugin(Bottle plugin) {
+		this.plugin = plugin;
+	}
+
+	public CommandExecutor(Bottle plugin) {
+		this.plugin = plugin;
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("bottle")) {
+			sender.sendMessage("Bottle is a major plugin for Faucitt servers.");
+			sender.sendMessage("~~~ List of Faucitt Commands ~~~");
+			for (Command c : plugin.commands) {
+				sender.sendMessage("/" + c.getName() + " - " + c.getDescription());
+			}
+		}
+		return false;
+	}
+}
